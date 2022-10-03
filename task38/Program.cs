ee6e94671f2f821b04081207c6406c83639abc85
Ivan Ustyantsev <vangold915@gmail.com>
2022-10-03 17:07:13 +0700

    Created FindDiffMaxMin function

--- task38/Program.cs
+++ task38/Program.cs
@@ -36,3 +36,13 @@ void PrintArray(double[] arr) {
      Console.WriteLine("");
 }
 
+int FindDiffMaxMin(double[] array) {
+    double max = array[0];
+    double min = array[0];
+    for (int i = 0; i < array.Length; i++) {
+        if (array[i] > max) max = array[i];
+        else if (array[i] < min) min = array[i];
+    }
+    double diff = max - min;
+    return diff;
+}
