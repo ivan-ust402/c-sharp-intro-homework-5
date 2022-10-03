c20e959a966433543f12c94298350e1304ee42be
Ivan Ustyantsev <vangold915@gmail.com>
2022-10-03 14:57:32 +0700

    Created CreateRandomArray function

---
+++ task34/Program.cs
@@ -0,0 +1,15 @@
+﻿/* 
+Задача 34: 
+Задайте массив заполненный случайными положительными трёхзначными числами или введите его через консоль. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2
+*/
+
+int[] CreateRandomArray(int length) {
+    int[] array = new int[length];
+    Random randNumGen = new Random();
+    for (int i = 0; i < array.Length; i++) {
+        array[i] = randNumGen.Next(99, 1000);
+    }
+    return array;
+}
