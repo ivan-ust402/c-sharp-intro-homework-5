24f0a96c8cd20bc4ee692428d4cc317acb87ae8e
Ivan Ustyantsev <vangold915@gmail.com>
2022-10-03 14:58:50 +0700

    Created PrintArray function

--- task34/Program.cs
+++ task34/Program.cs
@@ -13,3 +13,26 @@ int[] CreateRandomArray(int length) {
     }
     return array;
 }
+
+void PrintArray(int[] array) {
+    for (int i = 0; i < array.Length; i++) 
+    {
+        if (i == 0 && array.Length > 1) 
+        {
+            Console.Write($"[{array[i]}, ");
+        } 
+        else if (i == array.Length - 1 && array.Length > 1) 
+        {
+            Console.Write($"{array[i]}]");
+        } 
+        else if (i == 0 && array.Length <= 1) 
+        {
+            Console.Write($"[{array[i]}]");
+        }
+        else 
+        {
+            Console.Write($"{array[i]}, ");
+        }
+     }
+     Console.WriteLine("");
+}
