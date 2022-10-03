0f0bdf5429f2d18159f5c1f6171a7148116017b9
Ivan Ustyantsev <vangold915@gmail.com>
2022-10-03 17:41:47 +0700

    Created ParseToArray function

---
+++ task-with-star/Program.cs
@@ -0,0 +1,20 @@
+﻿/* 
+Задача со звёздочкой
+Найти в массиве самую длинную последовательность, состоящую из одинаковых элементов. Вывести на экран количество элементов самой длиной последовательности, элемент самой последовательности и номер элемента, который является ее началом.
+Данная задача не вилияет на оценку
+Пример:
+[1 2 2 4 4 4 4 3 3 3] ->
+Count: 4
+Element: 4
+Position: 3
+*/
+
+
+int[] ParseToArray(string str) {
+    string[] stringArray = str.Split(" ");
+    int[] numArray = new int[stringArray.Length];
+    for (int i = 0; i < stringArray.Length; i++) {
+        numArray[i] = int.Parse(stringArray[i]);
+    }
+    return numArray;
+}
