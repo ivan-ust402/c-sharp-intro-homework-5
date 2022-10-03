66068e5fe08ab62a7636c03c09b99bab25dc913c
Ivan Ustyantsev <vangold915@gmail.com>
2022-10-03 18:42:57 +0700

    Created FindChain function

--- task-with-star/Program.cs
+++ task-with-star/Program.cs
@@ -8,7 +8,11 @@ Count: 4
 Element: 4
 Position: 3
 */
-
+Console.Clear();
+Console.Write("Введите массив, последовательностей элементов. Элементы введите через пробел: ");
+string str = Console.ReadLine()!;
+int[] arr = ParseToArray(str);
+FindChain(arr);
 
 int[] ParseToArray(string str) {
     string[] stringArray = str.Split(" ");
@@ -17,4 +21,29 @@ int[] ParseToArray(string str) {
         numArray[i] = int.Parse(stringArray[i]);
     }
     return numArray;
+}
+
+int[] FindChain(int[] array) {
+    int currentCount = 0;
+    int prevCount = 0;
+    int element = array[0];
+    int position = 0;
+    int[] resultArray = new int[3];
+    
+    for (int i = 0; i < array.Length; i++) {
+        for (int j = i; j < array.Length; j++) {
+            if (array[i] == array[j]) currentCount++;
+        }
+        if (prevCount < currentCount) {
+            prevCount = currentCount;
+            element = array[i];
+            position = i;
+        }
+        currentCount = 0;
+    }
+    resultArray[0] = prevCount;
+    resultArray[1] = element;
+    resultArray[2] = position;
+    
+    return resultArray;
 }
