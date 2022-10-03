e0c33831b0103340a620f6ff6d469e46402f788b
Ivan Ustyantsev <vangold915@gmail.com>
2022-10-03 16:59:33 +0700

    Created PrintArray function

--- task38/Program.cs
+++ task38/Program.cs
@@ -13,4 +13,26 @@ int[] CreateArrayRealElem(int length) {
     return array;
 }
 
+void PrintArray(double[] arr) {
+        for (int i = 0; i < array.Length; i++) 
+    {
+        if (i == 0 && array.Length > 1) 
+        {
+            Console.Write($"[{array[i]}, ");
+        } 
+        else if (i == array.Length - 1 && array.Length > 1) 
+        {
+            Console.Write($"{array[i]}]");
+        } 
+        else if (i == 0 && array.Length <= 1) 
+        {
+            Console.Write($"[{array[i]}]");
+        }
+        else 
+        {
+            Console.Write($"{array[i]}, ");
+        }
+     }
+     Console.WriteLine("");
+}
 
