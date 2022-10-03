47eac46c18c4a189c1bb6e83ed450052ff928bef
Ivan Ustyantsev <vangold915@gmail.com>
2022-10-03 16:57:54 +0700

    Created CreateArrayRealElem function

---
+++ task38/Program.cs
@@ -0,0 +1,16 @@
+﻿/* 
+Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементом массива.
+
+[3 7 22 2 78] -> 76
+*/
+
+int[] CreateArrayRealElem(int length) {
+    double[] array = new double[length];
+    Random RandomGen = new Random();
+    for (int i = 0; i < array.Length; i++) {
+        array[i] = RandomGen.NextDouble();
+    }
+    return array;
+}
+
+
