b8f3b453c8b3847d5583f7e1d59e69eeb3205b85
Ivan Ustyantsev <vangold915@gmail.com>
2022-10-03 15:03:29 +0700

    Created FindAmountEvenNum function

--- task34/Program.cs
+++ task34/Program.cs
@@ -36,3 +36,13 @@ void PrintArray(int[] array) {
      }
      Console.WriteLine("");
 }
+
+int FindAmountEvenNum(int[] array) {
+ int count = 0;
+ for (int i = 0; i < array.Length; i++){
+    if (array[i] % 2 == 0) {
+        count++;
+    }
+ }
+ return count;
+}
