acbc26fd462282f22c1b26d621a0da3283cb7b76
Ivan Ustyantsev <vangold915@gmail.com>
2022-10-03 16:52:00 +0700

    Pre-completed task36

--- task36/Program.cs
+++ task36/Program.cs
@@ -5,9 +5,16 @@
 
 [-4, -6, 89, 6] -> 0
 */
+Console.Clear();
+Console.WriteLine("Вы находитесь в программе подсчета суммы всех элементов массива, находящихся на нечетных позициях.");
+Console.Write("Введите элементы массива через пробел: ");
+string str = Console.ReadLine()!;
+int[] arr = ParseToArray(str);
+int result = SumOfOddElem(arr);
+Console.WriteLine($"Сумма элементов массива на нечетных позициях равна {result}");
 
 int[] ParseToArray(string str) {
-    int[] stringArray = str.Split(" ");
+    string[] stringArray = str.Split(" ");
     int[] numArray = new int[stringArray.Length];
     for (int i = 0; i < stringArray.Length; i++) {
         numArray[i] = int.Parse(stringArray[i]);
@@ -16,9 +23,9 @@ int[] ParseToArray(string str) {
 }
 
 int SumOfOddElem(int[] array) {
+    int sum = 0;
     for (int i = 0; i < array.Length; i++) {
-        int sum = 0;
-        if (array[i] % 2 != 0) {
+        if (i % 2 != 0) {
             sum += array[i];
         }
     }
