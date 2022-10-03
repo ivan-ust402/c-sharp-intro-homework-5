ab46cc4cdd334744ad0fea147dc61f967e336229
Ivan Ustyantsev <vangold915@gmail.com>
2022-10-03 15:23:42 +0700

    Created ParseToArray function

---
+++ task36/Program.cs
@@ -0,0 +1,16 @@
+﻿/* 
+Задача 36: Задайте одномерный массив или введите через консоль, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+[3, 7, 23, 12] -> 19
+
+[-4, -6, 89, 6] -> 0
+*/
+
+int[] ParseToArray(string str) {
+    int[] stringArray = str.Split(" ");
+    int[] numArray = new int[stringArray.Length];
+    for (int i = 0; i < stringArray.Length; i++) {
+        numArray[i] = int.Parse(stringArray[i]);
+    }
+    return numArray;
+}
